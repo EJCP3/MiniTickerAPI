e0ce7d05d6600a99f75512bfb68f01d77eabf239
EJCP3 <euddycabrera03@gmail.com>
2025-12-22 10:54:12 -0400

    feat: creando ApplicationDbContext

---
+++ MiniTicker.Infrastructure.Persistence/ApplicationDbContext.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MiniTicker.Infrastructure.Persistence
+{
+    internal class ApplicationDbContext
+    {
+    }
+}
